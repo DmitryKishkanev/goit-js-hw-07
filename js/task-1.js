d191fce268eafeddfa5dbaabdf3711e796b9a308
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-03-11 22:33:37 +0200

    начал выполнять первое задание

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+const categoriesItemsByTagName = document.querySelectorAll("li.item");
+console.log(categoriesItemsByTagName);
+
+const categoriesItemsByTitle = document.querySelectorAll("h2");
+console.log(categoriesItemsByTitle.getAttribute("h2"));
